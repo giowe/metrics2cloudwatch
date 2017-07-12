94249e796216d45514346b0e17bb16b2fc4a4317
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2017-07-12 16:00:10 +0200

    mods

--- src/index.js
+++ src/index.js
@@ -5,23 +5,31 @@ const formatter = require('system-metrics-formatter');
 const zlib = require('zlib');
 
 exports.handler = (event, context, callback) => {
-  const { s3 } = event.Records[0];
-  console.log(s3);
+  const { bucket, object } = event.Records[0].s3;
   //Todo check if s3.key and s3.bucket exists
   const s3Client = new AWS.S3();
-  return s3Client.headObject({ key: s3.key, bucket: s3.bucket }).promise()
+
+  const params = {
+    Key: object.key,
+    Bucket: bucket.name
+  };
+  s3Client.headObject(params).promise()
     .then(data => {
-      const promises = [s3Client.getObject({ key: s3.key, bucket: s3.bucket }).promise()];
-      //Todo change lastKey
-      if(data.Metadata.lastKey) {
-        promises.push(s3Client.getObject({ key: data.lastKey, bucket: s3.bucket }).promise());
+      const { previouskey } = data.Metadata;
+      const promises = [s3Client.getObject(params).promise()];
+      if(previouskey) {
+        promises.push(s3Client.getObject({
+          Key: previouskey,
+          Bucket: bucket.name
+        }).promise());
       }
       return Promise.all(promises);
-    }).then(s3objects => {
-      const formatted = formatter(... s3objects.map(s3object => {
-        return zlib.unzipSync(s3object.Body);
-      }));
+    })
+    .then(s3objects => {
+      const formatted = formatter(...s3objects.map(s3object => JSON.parse(zlib.unzipSync(s3object.Body).toString())));
       const lastMetric = formatted[formatted.length - 1];
-      callback(null, event);
-    });
+      console.log(lastMetric);
+      callback(null, lastMetric);
+    })
+    .catch(err => callback(err));
 };
--- test-payload.js
+++ test-payload.js
@@ -1,16 +1,28 @@
 module.exports = {
   Records: [{
-    s3SchemaVersion: '1.0',
-    configurationId: 'metrics2cloudwatch',
-    bucket:
-      { name: 'sf-system-metrics',
-        ownerIdentity: { principalId: 'A1DVTYDKYT6EEE' },
-        arn: 'arn:aws:s3:::sf-system-metrics' },
-    object:
-      { key: 'matteo/matteo-pc-3/matteo_matteo-pc-3_1499855915255.json.gz',
-        size: 738,
-        eTag: 'b546ebeff480ccb52b9d003f8984aa1d',
-        sequencer: '005965FC2B78BD2E98'
+    eventVersion: '2.0',
+    eventSource: 'aws:s3',
+    awsRegion: 'eu-central-1',
+    eventTime: '2017-07-12T13:29:16.167Z',
+    eventName: 'ObjectCreated:Put',
+    userIdentity: { principalId: 'AWS:AIDAJ2CCRGODXMUQZK4FW' },
+    requestParameters: { sourceIPAddress: '144.76.87.112' },
+    responseElements:
+      { 'x-amz-request-id': '631780A8C708991F',
+        'x-amz-id-2': 'qTebm/kBTsrCLBGg+pCfO5DlMYJYCAGsqOQK+W+vpHaxE0uSCiVwmDs2T54sRZnOXWHX5VH/QIc=' },
+    s3:
+      { s3SchemaVersion: '1.0',
+        configurationId: 'metrics2cloudwatch',
+        bucket:
+          { name: 'sf-system-metrics',
+            ownerIdentity: [Object],
+            arn: 'arn:aws:s3:::sf-system-metrics' },
+        object:
+          { key: 'mow/web01/mow_web01_1499866155000.json.gz',
+            size: 1270,
+            eTag: '080fc2275ef52604f9150a2b12448831',
+            sequencer: '005966242C1139E4D3'
+          }
       }
   }]
 };
